1e05e768560ae28aec2e5b6b8b4be25d776f5647
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2024-11-06 10:05:48 +0100

    63

--- Source/SmashUE/Private/Match/MatchGameMode.cpp
+++ Source/SmashUE/Private/Match/MatchGameMode.cpp
@@ -5,6 +5,7 @@
 #include "Arena/ArenaPlayerStart.h"
 #include "Characters/SmashCharacter.h"
 #include "Kismet/GameplayStatics.h"
+#include "SmashUE/ArenaSettings.h"
 
 void AMatchGameMode::BeginPlay()
 {
@@ -42,19 +43,21 @@ void AMatchGameMode::FindPlayerStartActorsInArena(TArray<AArenaPlayerStart*>& Re
 TSubclassOf<ASmashCharacter> AMatchGameMode::GetSmashCharacterClassFromInputType(
 	EAutoReceiveInput::Type InputType) const
 {
+	const UArenaSettings* ArenaSettings = GetDefault<UArenaSettings>();
+	
 	switch (InputType)
 	{
 	case EAutoReceiveInput::Player0:
-		return SmashCharacterClassP0;
+		return ArenaSettings->SmashCharacterClassP0;
 		
 	case EAutoReceiveInput::Player1:
-		return SmashCharacterClassP1;
+		return ArenaSettings->SmashCharacterClassP1;
 		
 	case EAutoReceiveInput::Player2:
-		return SmashCharacterClassP2;
+		return ArenaSettings->SmashCharacterClassP2;
 		
 	case EAutoReceiveInput::Player3:
-		return SmashCharacterClassP3;
+		return ArenaSettings->SmashCharacterClassP3;
 		
 		default:
 			return nullptr;
--- Source/SmashUE/Public/Match/MatchGameMode.h
+++ Source/SmashUE/Public/Match/MatchGameMode.h
@@ -26,16 +26,4 @@ class SMASHUE_API AMatchGameMode : public AGameModeBase
 	protected:
 		UPROPERTY()
 		TArray<ASmashCharacter*> CharactersInsideArena;
-	
-		UPROPERTY(EditAnywhere)
-		TSubclassOf<ASmashCharacter> SmashCharacterClassP0;
-
-		UPROPERTY(EditAnywhere)
-		TSubclassOf<ASmashCharacter> SmashCharacterClassP1;
-
-		UPROPERTY(EditAnywhere)
-		TSubclassOf<ASmashCharacter> SmashCharacterClassP2;
-
-		UPROPERTY(EditAnywhere)
-		TSubclassOf<ASmashCharacter> SmashCharacterClassP3;
 };
